9241ad139bfb5da8a4067fdf2bc57a648101358a
senethmendis <mendis.seneth@gmail.com>
2024-06-30 18:01:57 +0530

    ui fixed

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -28,7 +28,7 @@ const Books = () => {
   return (
     <div>
       <h1 className="text-4xl my-5 font-bold">Book Shop</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
         {books.map((book) => (
           <div
             key={book.id}
